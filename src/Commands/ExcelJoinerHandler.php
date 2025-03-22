0b372e0ba33b7c161dde16615ed57bc51d2727d3
M Rohmani <rohmanie55@gmail.com>
2025-03-22 16:46:57 +0700

    fix: styling
    fix: styling

--- src/Commands/ExcelJoinerHandler.php
+++ src/Commands/ExcelJoinerHandler.php
@@ -6,17 +6,29 @@ use Mrohmani\ExcelJoiner\ExcelJoiner;
 
 class ExcelJoinerHandler extends Command
 {
-    protected $signature   = 'excel-join:run {source : The path to the source file or directory} {output : The path to save the output} {type?: The join type for operation row,sheet}';
+    protected $signature = 'excel-join:run 
+    {source : The path to the source file or directory} 
+    {output : The path to save the output} 
+    {type? : The join type for operation (row or sheet). Defaults to sheet}';
     protected $description = 'Execute excel joiner binary';
 
     public function handle()
     {
         $sourcePath = $this->argument('source');
         $outputPath = $this->argument('output');
-        $type = $this->argument('type','sheet');
-        $joiner = new ExcelJoiner($type);
-        $output = $joiner->execute($sourcePath, $outputPath);
+        $type = $this->argument('type')??'sheet';
 
-        $this->info("Excel joiner output: " . $output);
+        if (!in_array($type, ['row', 'sheet'])) {
+            $this->error("Invalid join type. Allowed values are 'row' or 'sheet'.");
+            return;
+        }
+
+        try {
+            $joiner = new ExcelJoiner($type);
+            $output = $joiner->execute($sourcePath, $outputPath);
+            $this->info("Excel joiner output: " . $output);
+        } catch (\Throwable $th) {
+            $this->error("Error: " . $th->getMessage());
+        }
     }
 }
